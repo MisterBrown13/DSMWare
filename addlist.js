c4b88cf09201c8f5c991050bac1f2ecc5bbeab18
Alexandre Brun <alexandrebrun.brun@gmail.com>
2014-05-31 20:32:31 +0200

    ajout de l'ajout de liste

--- addlist.js
+++ addlist.js
@@ -186,3 +186,67 @@ function edit_task(task_id)
     window.location = "http://localhost:81/DSMWare/edittask.html?id="+task_id;
 }
 
+$( "#add_list_form" ).submit(function( event ) {
+ //alert($('#add_task_form').serialize());
+  // Stop form from submitting normally
+  event.preventDefault();
+ alert("caca");
+
+dataString = $("#add_list_form").serialize();
+            //alert("ok");
+            //get the form data using another method
+            var list = $("input#list").val();
+	    
+            dataString = '{"title"="' + list +'"}';
+            alert(dataString);
+            //make the AJAX request, dataType is set to json
+            //meaning we are expecting JSON data in response from the server
+            $.ajax({
+                type: "POST",
+                url: "/me/lists",
+                data: dataString,
+                dataType: "json",
+                
+                //if received a response from the server
+                success: function( data, textStatus, jqXHR) {
+                    //our country code was correct so we have some information to display
+                     if(data.success){
+//                         $("#ajaxResponse").html("");
+//                         $("#ajaxResponse").append("<b>Country Code:</b> " + data.countryInfo.code + "");
+//                         $("#ajaxResponse").append("<b>Country Name:</b> " + data.countryInfo.name + "");
+//                         $("#ajaxResponse").append("<b>Continent:</b> " + data.countryInfo.continent + "");
+//                         $("#ajaxResponse").append("<b>Region:</b> " + data.countryInfo.region + "");
+//                         $("#ajaxResponse").append("<b>Life Expectancy:</b> " + data.countryInfo.lifeExpectancy + "");
+//                         $("#ajaxResponse").append("<b>GNP:</b> " + data.countryInfo.gnp + "");
+alert("oh yes");
+                     }
+                     //display error message
+                     else {
+                         //$("#ajaxResponse").html("<div><b>Country code in Invalid!</b></div>");
+			 alert("mmm");
+                     }
+                },
+                
+                //If there was no resonse from the server
+                error: function(jqXHR, textStatus, errorThrown){
+                     console.log("Something really bad happened " + textStatus);
+                      $("#ajaxResponse").html(jqXHR.responseText);
+                },
+                
+                //capture the request before it was sent to server
+                beforeSend: function(jqXHR, settings){
+                    //adding some Dummy data to the request
+                    //settings.data += "&dummyData=whatever";
+                    //disable the button until we get the response
+                    $('#add_list_button').attr("disabled", true);
+                },
+                
+                //this is called after the response or error functions are finsihed
+                //so that we can take some action
+                complete: function(jqXHR, textStatus){
+                    //enable the button
+                    $('#add_list_button').attr("disabled", false);
+                }
+      
+            });  
+});
