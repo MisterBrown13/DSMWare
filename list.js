990e1f409348b289a07af7b561da11078a6278e2
Alexandre Brun <alexandrebrun.brun@gmail.com>
2014-05-31 13:54:31 +0200

    ajout de fonction delete pas encore opé

--- list.js
+++ list.js
@@ -31,10 +31,14 @@ $(document).ready(function(){
    //$urldetest= "http://ip.jsontest.com/";
    $racine = "http://localhost:81/DSMWare/";
    $newdivtitle = '<div class="col-md-4">';
-   $insertdelete = '<button type="button" class="btn btn-default btn-lg">'+
-		    '<span class="glyphicon glyphicon-remove">'+'</span>'+ 'Remove'
+   $newdeletetask = '<button type="buttondeltask" onClick="deleteTask(this.id)" id="'; 
+   $enddeletetask = '" class="btn btn-default btn-lg">'+
+		    '<span class="glyphicon glyphicon-remove">'+'</span>'+ 'Remove task'
 		    '</button>'; 
-   $insertadd = '<button type="button" class="btn btn-default btn-lg">'+
+   $insertdeletelist = '<button type="buttondellist" onClick="deleteList(this.id)" class="btn btn-default btn-lg">'+
+   '<span class="glyphicon glyphicon-remove">'+'</span>'+ 'Remove list'
+   '</button>'; 
+   $insertadd = '<button type="buttonaddtask" onClick="addTask(this.id)" class="btn btn-default btn-lg">'+
 		    '<span class="glyphicon glyphicon-plus">'+'</span>'+ 'Add'+
 		   ' </button>';
    $newlink = '<a href="';
@@ -51,7 +55,7 @@ $(document).ready(function(){
        if($x)
 	{
 	    $i=$urlParams.id;
-	    $(".row").append($newdivtitle +"<h2>"+ $newlink + $linklistaddress + "?id=" + $i + $endlink + data[$i].title  + "</a>"+" " + $insertadd + " " + $insertdelete +  "</h2>" + $newul + $i + '">'+ $endul +$enddiv);
+	    $(".row").append($newdivtitle +"<h2>"+ $newlink + $linklistaddress + "?id=" + $i + $endlink + data[$i].title  + "</a>"+" " + $insertadd + " " + $newdeletetask + $i +$enddeletetask + " " + $insertdeletelist + "</h2>" + $newul + $i + '">'+ $endul +$enddiv);
 	    $.getJSON($racine+"me/tasks.json",function(data2)
 	    {
 		//alert(data2[$i].list_id);
@@ -64,6 +68,7 @@ $(document).ready(function(){
 			{
 			    //$(".list").append("<p>"+ caca +"</p>");
 			    $node=document.createElement("LI");
+			    $node.setAttribute("id", data2[$j].id);
 			    $textnode=document.createTextNode(data2[$j].title);
 			    $node.appendChild($textnode);
 			    document.getElementById("myList"+$i).appendChild($node);
@@ -79,10 +84,63 @@ $(document).ready(function(){
        $i = 0;
        while(data[$i])
 	{
-	  $(".row").append($newdivtitle + "<h2>"+ $newlink + $linklistaddress + "?id=" + $i + $endlink + data[$i].title  + "</a>"+" " + $insertadd + " " + $insertdelete +  "</h2>" +$enddiv);
+	  $(".row").append($newdivtitle + "<h2>"+ $newlink + $linklistaddress + "?id=" + $i + $endlink + data[$i].title  + "</a>" +  "</h2>" +$enddiv);
 	  $i++;
 	}
     }
+    
+    
+//	$("li").click(function(){
+//	    // récupérer l'id du li'
+//	    $id = this.id;
+//	    alert("ssfe");
+//	    //puis récupérer message avec getJSON
+//	  $.getJSON("http://localhost:81/DSMWare/me/"+$id+"/messages.json",function(data){
+//	  alert("caca");// puis afficher en dessous de la tache
+//	  //$("h1").append(" <b>"+ data.ip +"</b>.");
+//	  });
+//    });
      //alert(data[0].title);
+     
+//       $("button").click(function(){
+//	         alert("ip: " + this.type);
+//
+//    //recuperer le type puis recuperer l'id si necessaire (delete)'
+//    $.getJSON("http://ip.jsontest.com/",function(data){
+//      //$("h1").append(" <b>"+ data.ip +"</b>.");
+//    });
+//  });
+
    });
 });
+
+function deleteTask(task_id)
+{
+    alert("caca tu delete une task");
+    $.ajax({
+    url: "http://127.0.0.1:81/DSMWareServer/web/app_dev.php/me/task/"+list_id,
+    type: 'DELETE',
+    success: function(result) {
+        alert("ok");
+    }
+});
+}
+
+
+function deleteList(list_id)
+{
+    alert("caca delete list");
+   $.ajax({
+    url: "http://127.0.0.1:81/DSMWareServer/web/app_dev.php/me/list/"+list_id,
+    type: 'DELETE',
+    success: function(result) {
+        alert("ok");
+    }
+}); 
+    
+}
+
+function addTask(list_id)
+{
+    alert("ajout de tache");
+}
