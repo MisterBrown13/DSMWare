703c4c570f0876c1df6f38ad6ae7d550ce68edd7
Alexandre Brun <alexandrebrun.brun@gmail.com>
2014-06-02 15:26:23 +0200

    changement de la manière de s'authentifier + recup du token

--- post_auth.js
+++ post_auth.js
@@ -1,52 +1,77 @@
-function doFunction(){
-				var xhr_object = null; 
+
+
+$( "#login_form" ).submit(function( event ) {
+ //alert($('#add_task_form').serialize());
+  // Stop form from submitting normally
+  event.preventDefault();
+ //alert("caca");
+
+dataString = $("#login").serialize();
+            //alert("ok");
+            //get the form data using another method
+            var email = $("input#login").val();
+			var password = $("input#pwd").val();
+	    
+            dataString = 'email=' + email +'&password='+ password +'';
+            //alert(dataString);
+            //make the AJAX request, dataType is set to json
+            //meaning we are expecting JSON data in response from the server
+		$.ajaxSetup({
+		headers : {
+		'Content-Type' : "application/x-www-form-urlencoded"
+		}
+		});
+		alert('test');
+	    
+            $.ajax({
+                type: "POST",
+                url: "https://api.wunderlist.com/login/",
+                data: dataString,
+                dataType: "json",
+                
+                //if received a response from the server
+                success: function( data, textStatus, jqXHR) {
+                    //our country code was correct so we have some information to display
+                     if(data.token){
+//                         $("#ajaxResponse").html("");
+//                         $("#ajaxResponse").append("<b>Country Code:</b> " + data.countryInfo.code + "");
+//                         $("#ajaxResponse").append("<b>Country Name:</b> " + data.countryInfo.name + "");
+//                         $("#ajaxResponse").append("<b>Continent:</b> " + data.countryInfo.continent + "");
+//                         $("#ajaxResponse").append("<b>Region:</b> " + data.countryInfo.region + "");
+//                         $("#ajaxResponse").append("<b>Life Expectancy:</b> " + data.countryInfo.lifeExpectancy + "");
+//                         $("#ajaxResponse").append("<b>GNP:</b> " + data.countryInfo.gnp + "");
+$token = data.token;
+window.location = "http://localhost:81/DSMWare/list.html?token="+$token;
+                   }
+                     //display error message
+                     else {
+                         //$("#ajaxResponse").html("<div><b>Country code in Invalid!</b></div>");
 			 
-				if(window.XMLHttpRequest) {// Firefox 
-				   xhr_object = new XMLHttpRequest();
-					//alert("caca1");	
-				}					
-				else if(window.ActiveXObject){ // Internet Explorer 
-				   xhr_object = new ActiveXObject("Microsoft.XMLHTTP"); 
-					//alert("caca2");
-					}
-				else { // XMLHttpRequest non supporté par le navigateur 
-				   alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
-				   return; 
-				} 
-				//set
-				//$('#txt_name').val('bla');
-				var filename = "http://api.wunderlist.com/login/"; 
-				var s1       = $('#login').val(); 
-				var s2       = $('#pwd').val();
-				var data     = null; 
-				var token = null;
-				if(s1 != "" && s2 != "") {
-				   data = "email="+s1+"&password="+s2; 
-					//alert("caca rempli ;)");
-				 }
-				 
-				xhr_object.open("POST", filename, true); 
-				 
-				xhr_object.onreadystatechange = function() { 
-				   if(xhr_object.readyState == 4) { 
-					  var tmp = xhr_object.responseText.split(",");
-					  if(typeof(tmp[1]) != "undefined") { 
-						 //$('#login').val(tmp[1]) ;
-						 //$('#pwd').val(tmp[2]) ;
-						 //alert("big caca");
-					  } 
-					  var response = tmp[7].split(":");
-					  token = response[1].split("\"");
-					  //alert(token[1]); 
-					  //document.getElementById("token0").value = token[1];
-					  window.location.href = 'list.html?token='+token[1];
-				   } 
-				} 
-				 
-				   xhr_object.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
-				 
-				xhr_object.send(data);
-				//alert("caca");
-				
-				//return token[1];
-}
+			
+                     }
+                },
+                
+                //If there was no resonse from the server
+                error: function(jqXHR, textStatus, errorThrown){
+                     console.log("Something really bad happened " + textStatus);
+                      $("#ajaxResponse").html(jqXHR.responseText);
+					  
+                },
+                
+                //capture the request before it was sent to server
+                beforeSend: function(jqXHR, settings){
+                    //adding some Dummy data to the request
+                    //settings.data += "&dummyData=whatever";
+                    //disable the button until we get the response
+                    $('#login_button').attr("disabled", true);
+                },
+                
+                //this is called after the response or error functions are finsihed
+                //so that we can take some action
+                complete: function(jqXHR, textStatus){
+                    //enable the button
+                    $('#add_task_button').attr("disabled", false);
+                }
+      
+            });  
+});
