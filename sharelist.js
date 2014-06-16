963526ebec286b9ec6b4df67a85ce63a91a5c703
Alexandre Brun <alexandrebrun.brun@gmail.com>
2014-06-16 23:30:29 +0200

    ajout du share (non testé car ça fail sur le site)

--- sharelist.js
+++ sharelist.js
@@ -1,5 +1,5 @@
- 
- function parseURLParams(url) {
+     
+   function parseURLParams(url) {
     var queryStart = url.indexOf("?") + 1,
         queryEnd   = url.indexOf("#") + 1 || url.length + 1,
         query = url.slice(queryStart, queryEnd - 1),
@@ -26,8 +26,7 @@
  
  
 $(document).ready(function(){
-    
-   
+
    
    
    //$url = "http://127.0.0.1:81/DSMWareServer/web/app_dev.php/me/list/2";
@@ -52,26 +51,26 @@ $(document).ready(function(){
    $enddiv = '</div>';
    $newul = '<ul id="myList';
    $endul = '</ul>';
-   $x = location.search;
-   $urlParams = parseURLParams($x);   
+    $x = location.search;
+   $urlParams = parseURLParams($x);
    $.ajaxSetup({
     headers : {
     'Authorization' : 'Bearer '+$urlParams.token
     }
     });
+   
    $.getJSON($root+"me/lists",function(data)
    {
        $x = location.search;
        $urlParams = parseURLParams($x);
        //alert($urlParams.id);
-       //alert($urlParams.token);
-       $token = $urlParams.token;
-       if($urlParams.id)
+       if($x)
 	{
 	    $list_id=$urlParams.id;
+	    $token=$urlParams.token;
 	    //alert($list_id);
 	    $i = 0;
-	    //if(data){
+	    if(data){
 	    while(data[$i])
 		{
 		    if(data[$i].id==$list_id)
@@ -80,7 +79,7 @@ $(document).ready(function(){
 			}
 			$i++;
 		}
-	    //}
+	    }
 		$.ajaxSetup({
 		headers : {
 		'Authorization' : 'Bearer '+$token
@@ -92,8 +91,7 @@ $(document).ready(function(){
 		$j=0;
 		//alert($j);
 		//$(".row").append("<p>"+ caca +"</p>");
-		//if(data2)
-		//  {
+		if(data2){
 		while(data2[$j])
 		{
 		
@@ -110,13 +108,11 @@ $(document).ready(function(){
 //			    $node=document.createElement("LI");
 			    $node2=document.createElement("button");
 			    $node3=document.createElement("span");
-			    
 //			    $node.setAttribute("id", data2[$j].id);
 			    $node2.setAttribute("id", data2[$j].id);
 			    $node3.setAttribute("class", "glyphicon glyphicon-pencil");
 			    $node2.setAttribute("class","btn btn-default btn-lg");
 			    $node2.setAttribute("value","edit_one_task");
-			    
 //			    //$node2.setAttribute("type","edit_one_task");
 			    $node2.setAttribute("onClick","edit_task(this.id)");
 //			    $textnode=document.createTextNode(data2[$j].title);
@@ -124,64 +120,12 @@ $(document).ready(function(){
 //			    $node.appendChild($textnode);
 			    $node2.appendChild($textnode2);
 			    $node2.appendChild($node3);
-			    
 //			    document.getElementById("myList"+$list_id).appendChild($node);
 			    document.getElementById("myList"+$list_id).appendChild($node2);
-			   
-			    $node4=document.createElement("button");
-			    $node5=document.createElement("span");
-			    
-			    $node4.setAttribute("id", data2[$j].id);
-			    $node5.setAttribute("class", "glyphicon glyphicon-envelope");
-			    $node4.setAttribute("class","btn btn-default btn-lg");
-			    $node4.setAttribute("value","comment_one_task");
-			    
-			    $node4.setAttribute("onClick","comment_task(this.id)");
-			    
-			    $textnode3=document.createTextNode('Comments');
-			    
-			    $node4.appendChild($textnode3);
-			    $node4.appendChild($node5);
-			    document.getElementById("myList"+$list_id).appendChild($node4);
-			    
-			    
-			    $node6=document.createElement("button");
-			    $node7=document.createElement("span");
-			    
-			    $node6.setAttribute("id", data2[$j].id);
-			    $node7.setAttribute("class", "glyphicon glyphicon-envelope");
-			    $node6.setAttribute("class","btn btn-default btn-lg");
-			    $node6.setAttribute("value","comment_one_task");
-			    
-			    $node6.setAttribute("onClick","add_reminder(this.id)");
-			    
-			    $textnode4=document.createTextNode('Add reminder');
-			    
-			    $node6.appendChild($textnode4);
-			    $node6.appendChild($node7);
-			    document.getElementById("myList"+$list_id).appendChild($node6);
-			    
-			    
-			    $node8=document.createElement("button");
-			    $node9=document.createElement("span");
-			    
-			    $node8.setAttribute("id", data2[$j].id);
-			    $node9.setAttribute("class", "glyphicon glyphicon-envelope");
-			    $node8.setAttribute("class","btn btn-default btn-lg");
-			    $node8.setAttribute("value","share_one_task");
-			    
-			    $node8.setAttribute("onClick","share_list()");
-			    
-			    $textnode5=document.createTextNode('Share list');
-			    
-			    $node8.appendChild($textnode5);
-			    $node8.appendChild($node9);
-			    document.getElementById("row").appendChild($node8);
-		
 			}
 			$j++;
 		}
-		  //}
+		}
 		//alert($j);
 	    });
 	    
@@ -189,12 +133,13 @@ $(document).ready(function(){
 	else
 	 {
        $i = 0;
+       if(data){
        while(data[$i])
 	{
-	  $(".row").append($newdivtitle + "<h2>"+ $newlink + $linklistaddress + "?id=" + data[$i].id + '&token='+ $token +$endlink + data[$i].title  + "</a>" +  "</h2>" +$enddiv);
-
+	  $(".row").append($newdivtitle + "<h2>"+ $newlink + $linklistaddress + "?id=" + data[$i].id + $endlink + data[$i].title  + "</a>" +  "</h2>" +$enddiv);
 	  $i++;
 	}
+       }
     }
     
     
@@ -223,7 +168,7 @@ $(document).ready(function(){
 	 //alert(this.id);
 	 if(this.value == "remove_list")
 	     {
-		 $.ajaxSetup({
+		   $.ajaxSetup({
 		headers : {
 		'Authorization' : 'Bearer '+$token
 		}
@@ -242,20 +187,19 @@ $(document).ready(function(){
 	    {
 		// renvoyer sur une page avec formulaire 
 		// puis faire post /me/task avec les donn�es
-		window.location = "http://localhost:81/DSMWare/addtask.html?id="+this.id+'&token='+$token;
+		window.location = "http://localhost:81/DSMWare/addtask.html?id="+this.id+"&token="+$token;
 	    }
 	    
 	if(this.value == "delete_task")
 	    {
 		// renvoyer sur une page pour s�lectionner la tache 
 		// puis faire delete /me/task/task_id
-		window.location = "http://localhost:81/DSMWare/removetask.html?id="+this.id+'&token='+$token;
+		window.location = "http://localhost:81/DSMWare/removetask.html?id="+this.id+"&token="+$token;
 
 	    }
 	    
 	 if(this.value == "add_list")
 	     {
-
 		window.location = "http://localhost:81/DSMWare/addlist.html?token="+$token;
 	     }
      });
@@ -265,24 +209,75 @@ $(document).ready(function(){
 function edit_task(task_id)
 {
     $list_id=$urlParams.id;
-    window.location = "http://localhost:81/DSMWare/edittask.html?id="+task_id+'&token='+$token;
+    window.location = "http://localhost:81/DSMWare/edittask.html?id="+task_id+"&token="+$token;
 }
 
-function comment_task(task_id)
-{
-    $list_id=$urlParams.id;
-    window.location = "http://localhost:81/DSMWare/commenttask.html?id="+task_id+'&token='+$token;
-}
+$( "#share_list_form" ).submit(function( event ) {
+ //alert($('#add_task_form').serialize());
+  // Stop form from submitting normally
+  event.preventDefault();
+$list_id=$urlParams.id;
+dataString = $("#share_list_form").serialize();
+            //alert("ok");
+            //get the form data using another method
+            var email = $("input#email").val();
+	    
+            dataString = 'email=' + email;
+            alert(dataString);
+            //make the AJAX request, dataType is set to json
+            //meaning we are expecting JSON data in response from the server
+	    	 $.ajaxSetup({
+		headers : {
+		'Authorization' : 'Bearer '+$token
+		}
+		});
+            $.ajax({
+                type: "POST",
+                url: $root+$list_id+"/shares",
+                data: dataString,
+                dataType: "json",
+                
+                //if received a response from the server
+                success: function( data, textStatus, jqXHR) {
+                    //our country code was correct so we have some information to display
+                     if(data.id){
+//                         $("#ajaxResponse").html("");
+//                         $("#ajaxResponse").append("<b>Country Code:</b> " + data.countryInfo.code + "");
+//                         $("#ajaxResponse").append("<b>Country Name:</b> " + data.countryInfo.name + "");
+//                         $("#ajaxResponse").append("<b>Continent:</b> " + data.countryInfo.continent + "");
+//                         $("#ajaxResponse").append("<b>Region:</b> " + data.countryInfo.region + "");
+//                         $("#ajaxResponse").append("<b>Life Expectancy:</b> " + data.countryInfo.lifeExpectancy + "");
+//                         $("#ajaxResponse").append("<b>GNP:</b> " + data.countryInfo.gnp + "");
+		    window.location = "http://localhost:81/DSMWare/list.html?id="+$list_id+"&token="+$token;
 
-function add_reminder(task_id)
-{
-    $list_id=$urlParams.id;
-    window.location = "http://localhost:81/DSMWare/addreminder.html?id="+task_id+'&token='+$token;
-}
-
-function share_list()
-{
-    $list_id=$urlParams.id;
-    	   
-    window.location = "http://localhost:81/DSMWare/sharelist.html?id="+$list_id+'&token='+$token;
-}
+                     }
+                     //display error message
+                     else {
+                         //$("#ajaxResponse").html("<div><b>Country code in Invalid!</b></div>");
+			 alert("mmm");
+                     }
+                },
+                
+                //If there was no resonse from the server
+                error: function(jqXHR, textStatus, errorThrown){
+                     console.log("Something really bad happened " + textStatus);
+                      $("#ajaxResponse").html(jqXHR.responseText);
+                },
+                
+                //capture the request before it was sent to server
+                beforeSend: function(jqXHR, settings){
+                    //adding some Dummy data to the request
+                    //settings.data += "&dummyData=whatever";
+                    //disable the button until we get the response
+                    $('#add_list_button').attr("disabled", true);
+                },
+                
+                //this is called after the response or error functions are finsihed
+                //so that we can take some action
+                complete: function(jqXHR, textStatus){
+                    //enable the button
+                    $('#add_list_button').attr("disabled", false);
+                }
+      
+            });  
+});
